458d9a7d1fd4c499243bedb738d1be56a25d9387
TrevorEngland23 <trevor.england.523@gmail.com>
2023-11-08 15:47:19 -0800

    add try catch codeblock / final submission of day

--- Bank.java
+++ Bank.java
@@ -1,10 +1,14 @@
 /**
  * Bank
  */
+// when you use static it is a global method/variable. without static its considered an instance  method.
 
  enum AccountType {CHECKING, SAVINGS};
 public class Bank {
 
+    final String routingNumber = "8949249sosdjonODNF1";
+    // tracks overall balance stored at the bank.
+    private static double bankBalance; // static to make it a global variable. without static its an instance variable 
     AccountType accountType;
     private double balance;
     String authorizedUser;
@@ -24,6 +28,7 @@ public class Bank {
         this.balance = balance;
         this.authorizedUser = authorizedUser;
         this.accountType = AccountType.CHECKING;
+        bankBalance += balance;
     }
 
 
@@ -31,16 +36,27 @@ public class Bank {
         return balance;
     }
 
+    public static double getBankBalance(){
+        return bankBalance;
+    }
+
     // deposit method
     public double deposit(double value){
+        bankBalance += value;
         balance += value;
         return balance;
     }
 
     // withdrawl method
     public double withdrawal(double value){
+        bankBalance -= value;
         return balance -= value;
     }
 
+    // create a static method for currency conversion (USD -> AUD)
+
+    public static double convertUSDtoAUD(double usd){
+        return usd * 1.56;
+    }
 
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,43 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Bank trevorsBank = new Bank("Trevor England", 123.23);
+        Bank bank2 = new Bank("Amber", 500);
+        Bank bank3 = new Bank("Eric", 1000);
+
+        System.out.println(trevorsBank.getBalance());
+        System.out.println(bank2.getBalance());
+        System.out.println(bank3.getBalance());
+
+        System.out.println(trevorsBank.getBankBalance());
+        squareRoot();
         // trevorsBank.accountType = AccountType.CHECKING;
         // trevorsBank.deposit(50.00);
         // trevorsBank.withdrawal(25.20);
         // double amount = trevorsBank.getBalance();
         // System.out.println(amount);
 
-        System.out.println(trevorsBank.getBalance());
-        System.out.println(trevorsBank.accountType);
-        System.out.println(trevorsBank.authorizedUser);
+        // System.out.println(trevorsBank.getBalance());
+        // System.out.println(trevorsBank.accountType);
+        // System.out.println(trevorsBank.authorizedUser);
+
+        System.out.println(Bank.convertUSDtoAUD(2));;
     }
+
+    public static void squareRoot(){
+        try{
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+       int userInput = scanner.nextInt();
+       System.out.println("Square root is: " + Math.sqrt(userInput));
+        } catch(InputMismatchException e){
+            System.out.println("Please only enter integers.");
+        }
+
+    }
+    
 
     
 }
